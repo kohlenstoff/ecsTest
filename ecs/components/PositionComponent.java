4b0d8628a60ab3c937ea9a3c2bcea79554e374a7
kohlenstoff <49682563+kohlenstoff@users.noreply.github.com>
2019-04-17 09:06:14 +0200

    Create PositionComponent.java

---
+++ ecs/components/PositionComponent.java
@@ -0,0 +1,9 @@
+package ecsTest.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class PositionComponent implements Component {
+    public float x = 0.0f;
+    public float y = 0.0f;
+    public float z = 0.0f;
+}
