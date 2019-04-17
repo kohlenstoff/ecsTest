c5c26a0735b0b09f41c10762ce6a2f53ab3a01ae
kohlenstoff <49682563+kohlenstoff@users.noreply.github.com>
2019-04-17 09:07:00 +0200

    Create RenderableComponent.java

---
+++ ecs/components/RenderableComponent.java
@@ -0,0 +1,24 @@
+package ecsTest.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Disposable;
+
+public class RenderableComponent implements Component, Disposable {
+    Texture texture;
+    Sprite sprite;
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    @Override
+    public void dispose() {
+        sprite.getTexture().dispose();
+    }
+}
